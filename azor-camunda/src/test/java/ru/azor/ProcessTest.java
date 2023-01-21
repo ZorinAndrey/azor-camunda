7ff69fea7d4c082f24810e74c769437a52b9f043
Andrey Zorin <a.zorin@ylab.io>
2023-01-21 11:06:48 +0300

    3

--- azor-camunda/src/test/java/ru/azor/ProcessTest.java
+++ azor-camunda/src/test/java/ru/azor/ProcessTest.java
@@ -1,8 +1,9 @@
 package ru.azor;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+import org.assertj.core.api.SoftAssertions;
+import org.camunda.bpm.engine.HistoryService;
 import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.history.HistoricProcessInstance;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,19 +13,24 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import ru.azor.configuration.PostgreSqlContainerConfig;
 
+import java.util.List;
+
 /**
  * Tests for Camunda process.
  */
 
 @SpringBootTest
-@ContextConfiguration(classes =  PostgreSqlContainerConfig.class)
+@ContextConfiguration(classes = PostgreSqlContainerConfig.class)
 @Transactional
 class ProcessTest {
 
-    final String PROCESS_DEFINITION_KEY = "azor-camunda-process";
+    @Autowired
+    HistoryService historyService;
 
     @Autowired
     RuntimeService runtimeService;
+
+    private static final String PROCESS_DEFINITION_KEY = "azor-camunda-process";
 
     /**
      * Test for starting a process.
@@ -35,6 +41,16 @@ class ProcessTest {
 
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
 
-        assertNotNull(processInstance);
+        List<HistoricProcessInstance> historicProcessInstances = historyService.createHistoricProcessInstanceQuery()
+            .processInstanceId(processInstance.getProcessInstanceId()).list();
+
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        softAssertions.assertThat(processInstance).isNotNull();
+        softAssertions.assertThat(historicProcessInstances).hasSize(1);
+        softAssertions.assertThat(historicProcessInstances.get(0).getProcessDefinitionKey())
+            .isEqualTo(PROCESS_DEFINITION_KEY);
+
+        softAssertions.assertAll();
     }
 }
