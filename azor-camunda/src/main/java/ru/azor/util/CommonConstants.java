b3dceefa89af3dd860a8ae2ac673f337eb57dde2
Andrey Zorin <a.zorin@ylab.io>
2023-01-25 09:13:44 +0300

    fix_test

--- azor-camunda/src/main/java/ru/azor/util/CommonConstants.java
+++ azor-camunda/src/main/java/ru/azor/util/CommonConstants.java
@@ -12,7 +12,8 @@ public class CommonConstants {
     public static final String KAFKA_GROUP_ID = "azor-group";
 
     public static final String START_PARENT_PROCESS_MESSAGE_EVENT = "Message_start_main";
+    public static final String START_FIRST_SUB_PROCESS_MESSAGE_EVENT = "Message_start_first_sub";
+
     public static final String VARIABLE_TO_CHANGE = "variableToChange";
     public static final String VARIABLE_TO_CHANGE_VALUE = "Common";
-    public static final String START_FIRST_SUB_PROCESS_MESSAGE_EVENT = "Message_start_first_sub";
 }
